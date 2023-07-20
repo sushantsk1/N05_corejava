639791587702f1a8fc858ca8f6970f619abe8858
Alphabeetech <alphabeetech@gmail.com>
2023-07-20 12:05:08 +0530

    first commit odf userinput program

---
+++ dayone/src/org/tnsif/intro/UserInputDayOne.java
@@ -0,0 +1,40 @@
+package org.tnsif.intro;
+
+import java.util.Scanner;
+
+
+public class UserInputDayOne {
+
+	public static void main(String[] args) {
+		//program to demonstrate on user input
+		Scanner scan =new Scanner(System.in);
+		
+		//for int value
+		System.out.println("enter ur input");
+		int num=scan.nextInt();
+		System.out.println("here is your given value: "+num);
+		
+		//for float value
+		System.out.println("\nenter ur input");
+		float flt =scan.nextFloat();
+		System.out.println("here is your given value: "+flt);
+		
+		//for double
+		System.out.println("\nenter ur input");
+		double db=scan.nextDouble();
+		System.out.println("here is your given value: "+db);
+		
+		//for character
+		System.out.println("\nenter ur initaials");
+		char ch=scan.next().charAt(0);
+		System.out.println("here is your given value: "+ch);
+						
+		//for string
+		System.out.println("\nenter ur input");
+		String str=scan.next();
+		System.out.println("here is your given value: "+str);
+		
+		
+		
+	}
+}
