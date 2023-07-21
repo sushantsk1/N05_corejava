bf83f347a2d8094060fba75d198c58a4f0dcc5e3
Alphabeetech <alphabeetech@gmail.com>
2023-07-21 11:55:03 +0530

    third commit

---
+++ dayone/src/Homework/BloodDonation.java
@@ -0,0 +1,23 @@
+package Homework;
+import java.util.Scanner;
+public class BloodDonation {
+
+
+		public static void main(String[] args) {
+			Scanner scan =new Scanner(System.in);
+			
+			System.out.println("Enter Your Age: ");
+			int age=scan.nextInt();
+			
+			System.out.println("Enter Your Weight: ");
+			int weight=scan.nextInt();
+			
+			if(age>18&weight>50) {
+				System.out.println("you are eligible");
+			}
+			else
+				System.out.println("you are not eligible");
+		}
+
+	}
+
